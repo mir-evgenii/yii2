57a09638706dbaf282521611fba159a50983afc8
Eugene Mironov <mir-evgenii@mail.ru>
2019-05-12 02:40:59 +0300

    add main table

--- views/site/index.php
+++ views/site/index.php
@@ -9,21 +9,23 @@ echo $msg;
 
 // выводим категории первого уровня
 if(@$parents){
-    echo "<p>Категории</p>
-          <table>
+    echo "<h4>Категории</h4>
+	  <table class='table'>
+	  <thead class='thead-light'>
             <tr>
-                <td>Название</td>
-                <td></td>
-                <td></td>
-            <tr>
+                <th scope='col'>Название</th>
+                <th scope='col'></th>
+                <th scope='col'></th>
+	    <tr>
+	  </thead>
     ";
     foreach($parents as $parent){
-        echo "<tr>";
+        echo "<tbody><tr>";
         echo "<td><a href='index.php?r=site/index&id={$parent->parent['0']->id}'>".$parent->parent['0']->category."</a></td>
               <td><a href='index.php?r=site/change-category&id={$parent->parent['0']->id}'>Изменить</a></td>
               <td><a href='index.php?r=site/index&c_del={$parent->parent['0']->id}'>Удалить</a></td>
               ";
-        echo "</tr>";
+        echo "</tbody></tr>";
     }
     echo "</table>";
 }
@@ -32,21 +34,23 @@ if(@$parents){
 
 // выводим вложенные категории
 if(@$childs){
-    echo "<p>Категории вложенные в категорию \"{$cat}\"</p>
-          <table>
+    echo "<h4>Категории вложенные в категорию \"{$cat}\"</h4>
+          <table class='table'>
+	  <thead class='thead-light'>
             <tr>
-                <td>Название</td>
-                <td></td>
-                <td></td>
+                <th scope='col'>Название</th>
+                <th scope='col'></th>
+                <th scope='col'></th>
             <tr>
+	  </thead>
     ";
     foreach($childs as $child){
-        echo "<tr>";
+        echo "<tbody><tr>";
         echo "<td><a href='index.php?r=site/index&id={$child->child['0']->id}'>".$child->child['0']->category."</a></td>
               <td><a href='index.php?r=site/change-category&id={$child->child['0']->id}'>Изменить</a></td>
               <td><a href='index.php?r=site/index&c_del={$child->child['0']->id}'>Удалить</a></td>
               ";
-        echo "</tr>";
+        echo "</tbody></tr>";
     }
     echo "</table>";
 }
@@ -55,28 +59,30 @@ if(@$childs){
 
 // выводим товары
 if(@$goods){
-    echo "<p>Товары категории \"{$cat}\"</p>
-          <table>
+    echo "<h4>Товары категории \"{$cat}\"</h4>
+          <table class='table'>
+	  <thead class='thead-light'>
             <tr>
-                <td>Название</td>
-                <td>Цена</td>
-                <td>Кол-во</td>
-                <td></td>
-                <td></td>
-            </tr>";
+                <th scope='col'>Название</th>
+                <th scope='col'>Цена</th>
+                <th scope='col'>Кол-во</th>
+                <th scope='col'></th>
+                <th scope='col'></th>
+	    </tr>
+	  </thead>";
     foreach($goods as $good){
-        echo "<tr>";
+        echo "<tbody><tr>";
         echo "<td>".$good->goods['0']->good."</td>
               <td>".$good->goods['0']->price."</td>
               <td>".$good->goods['0']->number."</td>
               <td><a href='index.php?r=site/change-good&id={$good->goods['0']->id}'>Изменить</a></td>
               <td><a href='index.php?r=site/index&g_del={$good->goods['0']->id}'>Удалить</a></td>";
-        echo "</tr>";
+        echo "</tbody></tr>";
     }
     echo "</table>";
 }
 
 // если мы находимся во вложенной категории выводим ссылку на главную страницу
 if (@$cat) {
-    echo "<a href='index.php?r=site/index'>Назад</a>";
+    echo "<a class='btn btn-primary' href='index.php?r=site/index' role='button'>Назад</a>";
 }
