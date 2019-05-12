45ec68e10f2eb3a0e6a95f1fc4748863a5888e18
Eugene Mironov <mir-evgenii@mail.ru>
2019-05-12 03:29:14 +0300

    add create category form

--- views/site/create_category.php
+++ views/site/create_category.php
@@ -10,17 +10,19 @@ echo $input->field($form, 'category')->label('Имя новой категори
 echo $input->field($l_form, 'id_category_parent')->label('Номер родительской категории');
 
 // выводим список категорий
-echo "<ul>";
+echo "<select class='custom-select' id='inputGroupSelect01'>";
 foreach ($cat as $c){
-    echo "<li>Категория: {$c['id']} > \"{$c['category']}\"</li>";
+    echo "<option value='{$c['id']}'>{$c['category']}</option>";
 }
-echo "</ul>";
+echo "</select>";
 
-echo Html::submitButton('Создать категорию');
+// ссылка на главную страницу
+echo "<a class='btn btn-primary' href='index.php?r=site/index' role='button'>Назад</a>";
+
+echo Html::submitButton('Создать категорию', ['class' => 'btn btn-primary', 'type' => 'submit']);
+
 ActiveForm::end();
 
 // выводим сообщение об ошибке или успехе
 echo $msg;
 
-// ссылка на главную страницу
-echo "<a href='index.php?r=site/index'>Назад</a>";
